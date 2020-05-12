6e85f88a74d6faebd0d2038abbea6d0e95593ef2
FergyKay <bone201055@gmail.com>
2020-05-12 23:37:06 +0000

    wip: fixed android file issue

--- src/app/epub/epub.component.ts
+++ src/app/epub/epub.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { WebViewInterface } from 'nativescript-webview-interface';
 import { WebView } from 'tns-core-modules/ui/web-view/web-view';
+import { isAndroid } from 'tns-core-modules/ui/page/page';
 
 
 
@@ -10,33 +11,51 @@ import { WebView } from 'tns-core-modules/ui/web-view/web-view';
   styleUrls: ['./epub.component.css']
 })
 export class EpubComponent implements OnInit {
-  @ViewChild('webview',{static:true}) webview: ElementRef
+  @ViewChild('webview', { static: true }) webview: ElementRef
   private webViewInterface: WebViewInterface;
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-
- 
+  constructor() {
 
   }
 
-  wvloaded(){
-    this.webViewInterface = new WebViewInterface(this.webview.nativeElement ,
+  ngOnInit(): void {
+
+
+
+  }
+
+  wvloaded() {
+    this.webViewInterface = new WebViewInterface(this.webview.nativeElement,
       '~/app/www/epub.html');
-    this.webview.nativeElement.on(WebView.loadFinishedEvent, () => {
+
+      if(isAndroid){
+        let settings = this.webview.nativeElement.android.getSettings()
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+      }
       
+  
+    this.webview.nativeElement.on(WebView.loadFinishedEvent, () => {
+
+     
+
+
+
       this.webViewInterface.emit('loadBook', 'moby-dick.epub');
     });
   }
 
-  nextPage(){
-    this.webViewInterface.emit('nextPage','');
+  nextPage() {
+    this.webViewInterface.emit('nextPage', '');
   }
 
-  prevPage(){
-    this.webViewInterface.emit('prevPage','');
+  prevPage() {
+    this.webViewInterface.emit('prevPage', '');
   }
 
 }
